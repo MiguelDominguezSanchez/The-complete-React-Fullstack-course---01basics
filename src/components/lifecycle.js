aaccec8bb811966638d433eb169293a761479dbf
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-15 16:02:11 +0200

    016 Lifecycles

--- src/components/lifecycle.js
+++ src/components/lifecycle.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react'
 
 class Life extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = { action: '' }
+		console.log('1-Constructor')
+	}
+
+	static getDerivedFromProps() {
+		console.log('2-getDerivedFromProps')
+		return null
+	}
+
+	componentDidMount() {
+		console.log('4-componentDidMount')
+	}
+
+	componentWillUnmount() {
+		console.log('5-componentWillUnmount')
+	}
+
 	render() {
+		console.log('3-render')
 		return <div>LIFE</div>
 	}
 }

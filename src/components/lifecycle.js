5695137811990da88fc2abbb494e923926e99722
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-16 05:57:40 +0200

    017 Lifecycles part 2

--- src/components/lifecycle.js
+++ src/components/lifecycle.js
@@ -4,12 +4,18 @@ class Life extends Component {
 	constructor(props) {
 		super(props)
 
-		this.state = { action: '' }
+		this.state = { name: 'Francis' }
 		console.log('1-Constructor')
 	}
 
-	static getDerivedFromProps() {
+	static getDerivedFromProps(props, state) {
 		console.log('2-getDerivedFromProps')
+		if (state.name === 'Ron') {
+			return {
+				name: 'Milhouse',
+			}
+		}
+
 		return null
 	}
 
@@ -21,9 +27,35 @@ class Life extends Component {
 		console.log('5-componentWillUnmount')
 	}
 
+	shouldComponentUpdate(nextProps, nextState) {
+		console.log('x-shouldComponentUpdate')
+		if (nextState.name === 'Steve') {
+			return false
+		}
+		return true
+	}
+
+	getSnapshotBeforeUpdate(prevProps, prevState) {
+		let age = 20
+
+		return age
+	}
+
+	componentDidUpdate(prevProps, prevState, snapshot) {
+		console.log('x-componentDidUpdate')
+		// console.log(prevState)
+		// console.log(this.state)
+		console.log(snapshot)
+	}
+
 	render() {
 		console.log('3-render')
-		return <div>LIFE</div>
+		return (
+			<div>
+				<div>{this.state.name}</div>
+				<div onClick={() => this.setState({ name: 'Ron' })}>change name</div>
+			</div>
+		)
 	}
 }
 
